548a3f2222f7e3c4a456226ce683c15e72f7e69a
Nicolas Wicki <nwicki@ethz.ch>
2021-09-14 10:08:04 +0200

    1_dominoes: SOLVED

--- code_HS20/1_dominoes/src/main.cpp
+++ code_HS20/1_dominoes/src/main.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+// 1 ≤ n ≤ 10^6, 1 ≤ h_i < 2^31
 void testcase() {
+    int n,h; cin >> n >> h;
+    int sum = h-1;
+    int count = 1;
+    for(int i = 1; i < n; i++) {
+        cin >> h;
+        if(0 < sum) {
+            sum--;
+            count++;
+            sum = max(h-1,sum);
+        }
+    }
+    cout << count << endl;
 }
 
 int main(int argc, char const *argv[]) {
