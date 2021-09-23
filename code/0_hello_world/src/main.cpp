8f09b6038367e36dcba563a8caab611bf0ce53b2
Nicolas Wicki <nwicki@ethz.ch>
2021-09-23 22:07:57 +0200

    0_hello_world: SOLVED

--- code/0_hello_world/src/main.cpp
+++ code/0_hello_world/src/main.cpp
@@ -2,14 +2,9 @@
 
 using namespace std;
 
-void testcase() {
-}
 
 int main(int argc, char const *argv[]) {
     ios_base::sync_with_stdio(false);
-    int t; cin >> t;
-    for (int i = 0; i < t; ++i) {
-        testcase();
-    }
+    cout << "Hello World!" << endl;
     return 0;
 }
