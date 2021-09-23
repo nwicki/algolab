f1ad188d82bcd514204d0a8162e6832865b5aea9
Nicolas Wicki <nwicki@ethz.ch>
2021-09-23 22:07:46 +0200

    0_bfs: SOLVED

--- code/0_bfs/src/main.cpp
+++ code/0_bfs/src/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 void testcase() {
+    int n,m,v; cin >> n >> m >> v;
+    vector<vector<int>> graph(n,vector<int>());
+    for(int i = 0; i < m; i++) {
+        int a,b; cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    vector<int> distances(n,-1);
+    queue<int> to_visit;
+    vector<bool> visited(n,false);
+    to_visit.push(v);
+    distances[v] = 0;
+    while(!to_visit.empty()) {
+        int current = to_visit.front();
+        to_visit.pop();
+        visited[current] = true;
+        int new_distance = distances[current] + 1;
+        for(int neighbour : graph[current]) {
+            if(!visited[neighbour]) {
+                to_visit.push(neighbour);
+                int distance = distances[neighbour];
+                if(distance == -1 || new_distance < distance) {
+                    distances[neighbour] = new_distance;
+                }
+            }
+        }
+    }
+    for(int distance : distances) {
+        cout << distance << " ";
+    }
+    cout << endl;
 }
 
 int main(int argc, char const *argv[]) {
