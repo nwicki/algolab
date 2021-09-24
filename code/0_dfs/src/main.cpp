e7b0e19d4b3814c7fcf6944f2c99105121b789a7
Nicolas Wicki <nwicki@ethz.ch>
2021-09-24 15:26:40 +0200

    0_dfs: SOLVED

--- code/0_dfs/src/main.cpp
+++ code/0_dfs/src/main.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 void testcase() {
+    int n,m,v; cin >> n >> m >> v;
+    vector<vector<int>> graph(n,vector<int>());
+    for(int i = 0; i < m; i++) {
+        int a,b; cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    for(auto& nh : graph) {
+        sort(nh.begin(),nh.end());
+    }
+    vector<pair<int,int>> timestamps(n, make_pair(-1,-1));
+    stack<int> to_visit;
+    vector<bool> visited(n,false);
+    int timestamp = 0;
+    timestamps[v].first = timestamp++;
+    to_visit.push(v);
+    while(!to_visit.empty()) {
+        int current = to_visit.top();
+        visited[current] = true;
+        auto neighbours = graph[current];
+        auto it = neighbours.begin();
+        while(it != neighbours.end() && visited[*it]) {
+            it++;
+        }
+        if(it == neighbours.end()) {
+            timestamps[to_visit.top()].second = timestamp++;
+            to_visit.pop();
+        }
+        else {
+            timestamps[*it].first = timestamp++;
+            to_visit.push(*it);
+        }
+    }
+    string discovery;
+    string finishing;
+    for(auto df : timestamps) {
+        discovery += to_string(df.first) + " ";
+        finishing += to_string(df.second) + " ";
+    }
+    cout << discovery << endl;
+    cout << finishing << endl;
 }
 
 int main(int argc, char const *argv[]) {
