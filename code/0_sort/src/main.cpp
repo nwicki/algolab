4c5116c0e602b5ddf964e5e680dd96c05fd90bc8
Nicolas Wicki <nwicki@ethz.ch>
2021-09-24 15:59:11 +0200

    0_sort: SOLVED

--- code/0_sort/src/main.cpp
+++ code/0_sort/src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+bool cmp(int a, int b) {
+    return a > b;
+}
+
 void testcase() {
+    int n; cin >> n;
+    vector<int> uector(n);
+    for(int i = 0; i < n; i++) {
+        cin >> uector[i];
+    }
+    int x; cin >> x;
+    if(x) {
+        sort(uector.begin(),uector.end(),cmp);
+    }
+    else {
+        sort(uector.begin(),uector.end());
+    }
+    for(int i : uector) {
+        cout << i << " ";
+    }
+    cout << endl;
 }
 
 int main(int argc, char const *argv[]) {
