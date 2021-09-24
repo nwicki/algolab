59bb53f28f0c3bfb75a092b9d3d7f4a43c32c8ad
Nicolas Wicki <nwicki@ethz.ch>
2021-09-24 16:04:12 +0200

    0_strings: SOLVED

--- code/0_strings/src/main.cpp
+++ code/0_strings/src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 void testcase() {
+    string a,b; cin >> a >> b;
+    cout << a.size() << " " << b.size() << endl;
+    cout << a << b << endl;
+    reverse(a.begin(),a.end());
+    reverse(b.begin(),b.end());
+    char temp = a[0];
+    a[0] = b[0];
+    b[0] = temp,
+    cout << a << " " << b << endl;
 }
 
 int main(int argc, char const *argv[]) {
