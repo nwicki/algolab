e28a146b34cc1e9ddee2d61b3999e645127dfc15
Nicolas Wicki <nwicki@ethz.ch>
2021-09-24 15:48:58 +0200

    0_maps: SOLVED

--- code/0_maps/src/main.cpp
+++ code/0_maps/src/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
 void testcase() {
+    int q; cin >> q;
+    map<string,vector<int>> nap;
+    for(int i = 0; i < q; i++) {
+        int a; cin >> a;
+        string b; cin >> b;
+        auto it = nap.find(b);
+        if(a == 0) {
+            nap.erase(it);
+        }
+        else {
+            if(it == nap.end()) {
+                nap.insert(make_pair(b,vector<int>(1,a)));
+            }
+            else {
+                it->second.push_back(a);
+            }
+        }
+    }
+    string s; cin >> s;
+    auto it = nap.find(s);
+    if(it == nap.end()) {
+        cout << "Empty" << endl;
+    }
+    else {
+        sort(it->second.begin(),it->second.end());
+        for(int i : it->second) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main(int argc, char const *argv[]) {
