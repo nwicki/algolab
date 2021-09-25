30f016bf374e7e5ecfcf21107353e0a61182fcfb
Nicolas Wicki <nwicki@ethz.ch>
2021-09-25 12:35:34 +0200

    0_vectors: SOLVED

--- code/0_vectors/src/main.cpp
+++ code/0_vectors/src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void testcase() {
+    int n; cin >> n;
+    vector<int> uector(n);
+    for(int i = 0; i < n; i++) {
+        cin >> uector[i];
+    }
+    int d,a,b; cin >> d >> a >> b;
+    uector.erase(uector.begin()+d);
+    uector.erase(uector.begin()+a, uector.begin()+b+1);
+    if(uector.empty()) {
+        cout << "Empty" << endl;
+    }
+    else {
+        for(int i : uector) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main(int argc, char const *argv[]) {
