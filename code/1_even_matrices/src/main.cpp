507262089fc0f0711eba66bd69c841edb6da33fc
Nicolas Wicki <nwicki@ethz.ch>
2021-09-25 21:49:16 +0200

    0_even_matrices: SOLVED

--- code/1_even_matrices/src/main.cpp
+++ code/1_even_matrices/src/main.cpp
@@ -146,7 +146,7 @@ void reuse_even_matrices_3(const vector<vector<int>>& matrix) {
     // Define sums per row
     for(int i = 0; i < n; i++) {
         row_sums[i][0] = matrix[i][0];
-        for (int j = 0 + 1; j < n; j++) {
+        for (int j = 1; j < n; j++) {
             row_sums[i][j] = row_sums[i][j - 1] + matrix[i][j];
         }
     }
@@ -203,17 +203,107 @@ void reuse_even_matrices_3(const vector<vector<int>>& matrix) {
     cout << count << endl;
 }
 
+class even_status {
+public:
+    int horizontal_even;
+    int horizontal_odd;
+    int vertical_even;
+    int vertical_odd;
+    even_status() {
+        horizontal_even = 0;
+        horizontal_odd = 0;
+        vertical_even = 0;
+        vertical_odd = 0;
+    }
+};
+
+// Use approach from even_pairs
+void reuse_even_matrices_4(const vector<vector<int>>& matrix) {
+    int n = (int) matrix.size();
+    int result = 0;
+    for(int start = 0; start < n; start++) {
+        vector<vector<int>> sums(n,vector<int>(n));
+        sums[start][start] = matrix[start][start];
+        for(int j = start + 1; j < n; j++) {
+            sums[start][j] = sums[start][j-1] + matrix[start][j];
+        }
+        for(int j = start+1; j < n; j++) {
+            sums[j][start] = sums[j-1][start] + matrix[j][start];
+        }
+        for(int i = start+1; i < n; i++) {
+            sums[i][i] = matrix[i][i] + sums[i][i-1] + sums[i-1][i] - sums[i-1][i-1];
+            for(int j = i+1; j < n; j++) {
+                sums[i][j] = sums[i][j-1] + matrix[i][j] + sums[i-1][j] - sums[i-1][j-1];
+            }
+            for(int j = i+1; j < n; j++) {
+                sums[j][i] = sums[j-1][i] + matrix[j][i] + sums[j][i-1] - sums[j-1][i-1];
+            }
+        }
+        vector<vector<even_status>> even_odds(n,vector<even_status>(n));
+        for(int i = start; i < n; i++) {
+            int even = 0;
+            int odd = 0;
+            for(int j = n-1; j > i; j--) {
+                int is_even = sums[i][j] % 2 == 0;
+                even += is_even;
+                odd += !is_even;
+                even_odds[i][j-1].horizontal_even = even;
+                even_odds[i][j-1].horizontal_odd = odd;
+            }
+            even = 0;
+            odd = 0;
+            for(int j = n-1; j > i; j--) {
+                int is_even = sums[j][i] % 2 == 0;
+                even += is_even;
+                odd += !is_even;
+                even_odds[j-1][i].vertical_even = even;
+                even_odds[j-1][i].vertical_odd = odd;
+            }
+        }
+        for(int i = n-1; i > start; i--) {
+            int even = 0;
+            int odd = 0;
+            for(int j = n-1; j > start; j--) {
+                int is_even = sums[i][j] % 2 == 0;
+                even += is_even;
+                odd += !is_even;
+                even_odds[i][j-1].horizontal_even = even;
+                even_odds[i][j-1].horizontal_odd = odd;
+            }
+            even = 0;
+            odd = 0;
+            for(int j = n-1; j > start; j--) {
+                int is_even = sums[j][i] % 2 == 0;
+                even += is_even;
+                odd += !is_even;
+                even_odds[j-1][i].vertical_even = even;
+                even_odds[j-1][i].vertical_odd = odd;
+            }
+        }
+        for(int i = start; i < n; i++) {
+            for(int j = start; j < n; j++) {
+                if(sums[i][j] % 2 == 0) {
+                    result += even_odds[i][j].horizontal_even + even_odds[i][j].vertical_even + 1;
+                }
+                else {
+                    result += even_odds[i][j].horizontal_odd + even_odds[i][j].vertical_odd;
+                }
+            }
+        }
+    }
+    cout << result << endl;
+}
+
 void testcase() {
     int n; cin >> n;
     vector<vector<int>> matrix(n);
     for(int i = 0; i < n; i++) {
         matrix[i] = vector<int>(n);
         for(int j = 0; j < n; j++) {
-            int xij; cin >> xij;
-            matrix[i][j] = xij;
+            cin >> matrix[i][j];
         }
     }
-    reuse_even_matrices_3(matrix);
+    reuse_even_matrices_4(matrix);
 }
 
 int main(int argc, char const *argv[]) {
