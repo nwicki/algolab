d8c2550fe92cba9404cf31e195bebc46d92319c4
Nicolas Wicki <nwicki@ethz.ch>
2021-09-25 12:00:08 +0200

    0_sum_it: SOLVED

--- code/0_sum_it/src/main.cpp
+++ code/0_sum_it/src/main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 void testcase() {
+    int n; cin >> n;
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        int a; cin >> a;
+        sum += a;
+    }
+    cout << sum << endl;
 }
 
 int main(int argc, char const *argv[]) {
