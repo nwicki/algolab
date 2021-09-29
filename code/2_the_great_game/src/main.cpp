f3b810c991fa7319967ba2bd251d2c32a1bec46a
Nicolas Wicki <nwicki@ethz.ch>
2021-09-29 18:44:27 +0200

    Week 2: Base

---
+++ code/2_the_great_game/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void testcase() {
+
+}
+
+int main(int argc, char const *argv[]) {
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        testcase();
+    }
+    return 0;
+}
