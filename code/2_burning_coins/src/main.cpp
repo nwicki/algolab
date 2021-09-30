4d59fc3deb0e5de16b69c2e4dd8edc808f21e7af
Nicolas Wicki <nwicki@ethz.ch>
2021-09-30 11:06:03 +0200

    2_burning_coins: SOLVED

--- code/2_burning_coins/src/main.cpp
+++ code/2_burning_coins/src/main.cpp
@@ -3,8 +3,34 @@
 
 using namespace std;
 
-void testcase() {
+vector<vector<int>> memo;
+vector<int> coins;
+int n;
 
+int get_max(int i, int j) {
+    if(j < i) {
+        return 0;
+    }
+    if(i == j) {
+        return coins[i];
+    }
+    int result = memo[i][j];
+    if(result != -1) {
+        return result;
+    }
+    result = max(coins[i] + min(get_max(i+2,j), get_max(i+1,j-1)), coins[j] + min(get_max(i+1,j-1), get_max(i,j-2)));
+    memo[i][j] = result;
+    return result;
+}
+
+void testcase() {
+    cin >> n;
+    coins = vector<int>(n);
+    for(int& e : coins) {
+        cin >> e;
+    }
+    memo = vector<vector<int>>(n,vector<int>(n,-1));
+    cout << get_max(0,n-1) << endl;
 }
 
 int main(int argc, char const *argv[]) {
