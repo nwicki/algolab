d79ebe61baa7777634c0775972f512f9f101c389
Nicolas Wicki <nwicki@ethz.ch>
2021-10-04 20:46:42 +0200

    3_from_russia_with_love: SOLVED

--- code/3_from_russia_with_love/src/main.cpp
+++ code/3_from_russia_with_love/src/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-const int MAX = (2L << 31) - 1;
+const int MAX = (1L << 31) - 1;
 
 vector<int> coins;
 vector<vector<vector<int>>> memo;
@@ -48,9 +48,47 @@ void compute_20(int n, int m, int k) {
   cout << pick_a_coin(0,n-1,0,k,m) << endl;
 }
 
+const int MIN = 1 << 31;
+vector<vector<int>> memo_60;
+
+int pick_a_coin_60(int i, int j, int step) {
+    int mem = memo_60[i][j];
+    if(mem != -1) {
+        return mem;
+    }
+    if(i == j) {
+        mem = coins[i];
+    }
+    else if(j - i <= step) {
+        mem = max(coins[i],coins[j]);
+    }
+    else {
+        int result_left = MAX;
+        int end = j + 1;
+        for(int ii = i+1, jj = j - step; jj < end; ii++,jj++) {
+            result_left = min(result_left, pick_a_coin_60(ii, jj, step));
+        }
+        result_left += coins[i];
+        int result_right = MAX;
+        end = j;
+        for(int ii = i, jj = j - step - 1; jj < end; ii++,jj++) {
+            result_right = min(result_right, pick_a_coin_60(ii, jj, step));
+        }
+        result_right += coins[j];
+        mem = max(result_left,result_right);
+    }
+    memo_60[i][j] = mem;
+    return mem;
+}
+
 void compute_60(int n, int m, int k) {
-  vector<vector<pair<int,int>>> rounds;
-  cout << pick_a_coin(0,n-1,0,k,m) << endl;
+    memo_60 = vector<vector<int>>(n,vector<int>(n,-1));
+    int result = MAX;
+    int step = m - 1;
+    for(int i = 0,j = n - 1 - k; j < n; i++,j++) {
+        result = min(result, pick_a_coin_60(i, j, step));
+    }
+    cout << result << endl;
 }
 
 void testcase() {
@@ -59,7 +97,7 @@ void testcase() {
   for(int& e : coins) {
     cin >> e;
   }
-  compute_20(n,m,k);
+  compute_60(n,m,k);
 }
 
 int main(int argc, char** argv) {
