1615e084df8a475612dba67e075d48b86814d933
Nicolas Wicki <nwicki@ethz.ch>
2021-10-07 21:44:13 +0200

    3_antenna: SOLVED

---
+++ code/3_antenna/src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <CGAL/number_utils.h>
+#include <CGAL/Min_circle_2_traits_2.h>
+#include <CGAL/Min_circle_2.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt K;
+typedef CGAL::Min_circle_2_traits_2<K> Traits;
+typedef CGAL::Min_circle_2<Traits> Min_circle;
+
+double floor_to_double(const K::FT& x) {
+  double a = floor(CGAL::to_double(x));
+  while (a > x) a -= 1;
+  while (a+1 <= x) a += 1;
+  return a;
+}
+
+void testcase(int n) {
+    vector<K::Point_2> citizens(n);
+    for(auto& c : citizens) {
+        double x,y; cin >> x >> y;
+        c = K::Point_2(x,y);
+    }
+    shuffle(citizens.begin(), citizens.end(), default_random_engine((unsigned) CGAL::to_double(citizens[0].x())));
+    Min_circle mc(citizens.begin(), citizens.end());
+    cout << ceil(CGAL::sqrt(mc.circle().squared_radius())) << endl;
+}
+
+int main(int argc, char** argv) {
+    ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+    while(n) {
+        testcase(n);
+        cin >> n;
+    }
+}
