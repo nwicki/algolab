32bc4aee1d997e03e6b170b8aa4dfec72e06abf6
Nicolas Wicki <nwicki@ethz.ch>
2021-10-24 10:17:32 +0200

    5_moving_books: cleanup

--- code/5_moving_books/src/main.cpp
+++ code/5_moving_books/src/main.cpp
@@ -7,54 +7,6 @@
 
 using namespace std;
 
-bool cmp_weights(const pair<int,bool>& a, const pair<int,bool>& b) {
-    return a.first > b.first;
-}
-
-void test1() {
-    int n, m; cin >> n >> m;
-    vector<int> strengths(n);
-    for(int& s : strengths) {
-        cin >> s;
-    }
-    sort(strengths.begin(), strengths.end(), greater<>());
-    vector<pair<int,bool>> weights(m);
-    for(auto& w : weights) {
-        cin >> w.first;
-        w.second = true;
-    }
-    sort(weights.begin(), weights.end(), cmp_weights);
-    if(strengths.front() < weights.front().first) {
-        cout << "impossible" << endl;
-        return;
-    }
-    int time = 0;
-    int count = 0;
-    while(count != m) {
-        int i = 0;
-        int j = 0;
-        while(i < n && j < m) {
-            if(weights[j].second) {
-                if(weights[j].first <= strengths[i]) {
-                    weights[j].second = false;
-                    count++;
-                    i++;
-                    j++;
-                }
-                else {
-                    j++;
-                }
-            }
-            else {
-                j++;
-            }
-        }
-        time += 3;
-    }
-    time--;
-    cout << time << endl;
-}
-
 class csr {
 public:
     int cycle;
@@ -74,7 +26,7 @@ public:
     }
 };
 
-void test2() {
+void testcase() {
     int n, m; cin >> n >> m;
     vector<int> strengths(n);
     for(int& s : strengths) {
@@ -90,75 +42,35 @@ void test2() {
         cout << "impossible" << endl;
         return;
     }
-    vector<pair<int,int>> condensed_strengths;
-    pair<int,int> current_strength = make_pair(0,strengths.front());
-    for(const auto& s : strengths) {
-        if(s == current_strength.second) {
-            current_strength.first++;
-        }
-        else {
-            condensed_strengths.push_back(current_strength);
-            current_strength = make_pair(1,s);
-        }
-    }
-    condensed_strengths.push_back(current_strength);
-    vector<pair<int,int>> condensed_weights;
-    pair<int,int> current_weight = make_pair(0,weights.front());
-    for(const auto& w : weights) {
-        if(w == current_weight.second) {
-            current_weight.first++;
-        }
-        else {
-            condensed_weights.push_back(current_weight);
-            current_weight = make_pair(1, w);
-        }
-    }
-    condensed_weights.push_back(current_weight);
-    vector<int> strengths_to_weights(condensed_weights.size(),0);
-    for(size_t i = 0, j = 0; i < condensed_weights.size(); i++) {
-        int strength_amount = 0;
-        while(j < condensed_strengths.size() && condensed_weights[i].second <= condensed_strengths[j].second) {
-            strength_amount += condensed_strengths[j].first;
-            j++;
-        }
-        strengths_to_weights[i] = strength_amount;
-    }
-    vector<pair<int,int>> sws;
-    pair<int,int> current_sws(0,0);
-    for(auto its = strengths.begin(), itw = weights.begin(); its != strengths.end() || itw != weights.end();) {
+    vector<pair<int,int>> strengths_weights;
+    pair<int,int> current(0, 0);
+    auto its = strengths.begin(), itw = weights.begin();
+    while(its != strengths.end() && itw != weights.end()) {
         while(its != strengths.end() && *itw <= *its) {
-            current_sws.first++;
+            current.first++;
             its++;
         }
-        if(its == strengths.end()) {
-            while (itw != weights.end()) {
-                current_sws.second++;
-                itw++;
-            }
+        while (itw != weights.end() && *its < *itw) {
+            current.second++;
+            itw++;
         }
-        else {
-            while (itw != weights.end() && *its < *itw) {
-                current_sws.second++;
-                itw++;
-            }
-        }
-        sws.push_back(current_sws);
-        current_sws = make_pair(0,0);
+        strengths_weights.push_back(current);
+        current = make_pair(0, 0);
     }
-    vector<pair<int,int>> strengths_weights;
-    pair<int,int> current_sw = make_pair(strengths_to_weights[0],condensed_weights[0].first);
-    for(size_t i = 1; i < strengths_to_weights.size(); i++) {
-        if(strengths_to_weights[i] != 0) {
-            strengths_weights.push_back(current_sw);
-            current_sw = make_pair(0,0);
+    if(itw != weights.end() || its != strengths.end()) {
+        while(its != strengths.end()) {
+            current.first++;
+            its++;
         }
-        current_sw.first += strengths_to_weights[i];
-        current_sw.second += condensed_weights[i].first;
+        while (itw != weights.end()) {
+            current.second++;
+            itw++;
+        }
+        strengths_weights.push_back(current);
     }
-    strengths_weights.push_back(current_sw);
     int cycles = 0;
     priority_queue<csr, vector<csr>, cmp_csr> remaining;
-    for(const auto& sw : sws) {
+    for(const auto& sw : strengths_weights) {
         int w = sw.second;
         int c = 0;
         remaining.push(csr(0, sw.first, true));
@@ -188,6 +100,6 @@ int main() {
     ios_base::sync_with_stdio(false);
     int t; cin >> t;
     for(int i = 0; i < t; i++) {
-        test2();
+        testcase();
     }
 }
