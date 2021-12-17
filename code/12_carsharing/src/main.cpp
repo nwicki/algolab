dd4fa81089453c1a6c5b9be4de10f4f09158c3b1
Nicolas Wicki <nwicki@ethz.ch>
2021-12-17 16:12:27 +0100

    12_carsharing: Before rewrite

--- code/12_carsharing/src/main.cpp
+++ code/12_carsharing/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <map>
 #include <algorithm>
 #include <boost/graph/adjacency_list.hpp>
@@ -19,6 +20,8 @@ typedef adjacency_list<vecS, vecS, directedS, no_property,
                 property<edge_weight_t, long>>>>> graph_t;
 typedef graph_traits<graph_t>::edge_descriptor edge_t;
 typedef graph_traits<graph_t>::vertex_descriptor vertex_t;
+
+const bool PRINT = false;
 
 void add_edge(graph_t& g, vertex_t u, vertex_t v, long capacity, long cost) {
     auto c_map = get(edge_capacity, g);
@@ -43,7 +46,25 @@ public:
     long p;
 };
 
-const bool PRINT = false;
+void get_paths(const vector<vector<vertex_t>>& g, vertex_t source, vertex_t target, vector<vector<vertex_t>>& res) {
+    deque<pair<vertex_t,size_t>> to_visit;
+    vector<vector<vertex_t>> paths = vector<vector<vertex_t>>({ vector<vertex_t>({ source }) });
+    to_visit.emplace_back(source,0);
+    while(!to_visit.empty()) {
+        auto current = to_visit.front();
+        to_visit.pop_front();
+        auto neighbours = g[current.first];
+        for(auto n : neighbours) {
+            paths.push_back(paths[current.second]);
+            paths.back().push_back(n);
+            to_visit.emplace_back(n, paths.size()-1);
+        }
+    }
+    for(auto& p : paths) {
+        if(p.back() != target) continue;
+        res.push_back(p);
+    }
+}
 
 void testcase() {
     size_t n, s; cin >> n >> s;
@@ -70,39 +91,119 @@ void testcase() {
         }
     }
     graph_t g(vertices);
-    const long adjust = 0;
+    const long adjust = 100;
     const long MAX = 1L << 62;
     vertex_t source = add_vertex(g);
     vertex_t target = add_vertex(g);
+    vector<vector<vertex_t>> path_graph(vertices+2);
     for(size_t i = 0; i < s; i++) {
         sort(indices_sorted[i].begin(), indices_sorted[i].end());
-        auto ind = indices_sorted[i];
-        add_edge(g, source, ind.front().second, cars[i], 0);
-        if(PRINT) cout << "Station " << i << ": ";
-        for(auto& e : ind) {
-            if(PRINT) cout << "(" << e.first << "," << e.second << "), ";
-        }
-        if(PRINT) cout << endl;
-        if(PRINT) cout << "Source " << source << " to station " << i << ": (" << ind.begin()->first << "," << ind.begin()->second << ")" << endl;
-        add_edge(g, ind.back().second, target, MAX, 0);
-        if(PRINT) cout << "Station " << i << " to target " << target << ": (" << ind.back().first << "," << ind.back().second << ")" << endl;
+        vertex_t v = indices_sorted[i].front().second;
+        path_graph[source].push_back(v);
+        add_edge(g, source, v, cars[i], adjust);
+        vertex_t u = indices_sorted[i].back().second;
+        path_graph[u].push_back(target);
+        add_edge(g, u, target, MAX, adjust);
     }
     for(auto& ind : indices_sorted) {
         for(size_t j = 0; j < ind.size()-1; j++) {
-            add_edge(g, ind[j].second, ind[j+1].second, MAX, adjust);
-            if(PRINT) cout << "Station connection: (" << ind[j].first << "," << ind[j].second << ") to (" << ind[j+1].first << "," << ind[j+1].second << ")" << endl;
+            vertex_t u = ind[j].second;
+            vertex_t v = ind[j+1].second;
+            path_graph[u].push_back(v);
+            if(j+1 == ind.size()-1) {
+                add_edge(g, u, v, MAX, adjust);
+            }
+            else {
+                add_edge(g, u, v, MAX, adjust);
+            }
         }
     }
     for(auto& r : routes) {
         size_t start = indices[r.u].find(r.d)->second;
         size_t end = indices[r.v].find(r.a)->second;
+        path_graph[start].push_back(end);
         add_edge(g, start, end, 1, -r.p + adjust);
-        if(PRINT) cout << "Route: (" << indices[r.u].find(r.d)->first << "," << indices[r.u].find(r.d)->second << ") to (" << indices[r.v].find(r.a)->first << "," << indices[r.v].find(r.a)->second << ") with " << 1 << "," << -r.p + adjust << endl;
     }
-    push_relabel_max_flow(g, source, target);
-    cycle_canceling(g);
-//    successive_shortest_path_nonnegative_weights(g, source, target);
-    cout << -find_flow_cost(g) << endl;
+    vector<vector<vertex_t>> paths;
+    get_paths(path_graph, source, target, paths);
+    vector<pair<long,vector<edge_t>>> edge_paths;
+    edge_paths.reserve(paths.size());
+    size_t max_length = 0;
+    for(const auto& p : paths) {
+        max_length = max(max_length, p.size()-1);
+        vector<edge_t> edge_path;
+        edge_path.reserve(p.size()-1);
+        for(size_t i = 0; i < p.size()-1; i++) {
+            auto res = edge(p[i], p[i+1], g);
+            if(!res.second) {
+                cout << "Edge somehow does not exist!" << endl;
+                exit(1);
+            }
+            edge_path.push_back(res.first);
+        }
+        edge_paths.emplace_back(adjust * edge_path.size(),edge_path);
+    }
+    map<edge_t, long> weight_adjust;
+    graph_traits<graph_t>::edge_iterator it, end;
+    for(tie(it,end) = edges(g); it != end; it++) {
+        weight_adjust[*it] = adjust;
+    }
+    long path_val = (long) max_length * adjust;
+    map<edge_t, bool> handled;
+    size_t finished = 0;
+    for(tie(it,end) = edges(g); it != end; it++) {
+        handled[*it] = false;
+    }
+    for(const auto& p : edge_paths) {
+        if(path_val == p.first) {
+             for(const auto& e : p.second) {
+                 if(!handled[e]) {
+                     handled[e] = true;
+                     finished++;
+                 }
+            }
+        }
+    }
+    auto w_map = get(edge_weight, g);
+    auto r_map = get(edge_reverse, g);
+    cout << "Handled: ";
+    size_t n_edges = 0;
+    for(tie(it,end) = edges(g); it != end; it++) {
+        if(handled[*it]) {
+            cout << *it << " " << w_map[*it] << " ";
+        }
+        n_edges++;
+    }
+    cout << endl;
+    n_edges /= 2;
+    cout << "Amount of edges: " << n_edges << endl;
+    while(finished != n_edges) {
+        for(auto& p : edge_paths) {
+            vector<edge_t> update;
+            for(auto& e : p.second) {
+                if(!handled[e]) {
+                    update.push_back(e);
+                }
+            }
+            p.second = update;
+        }
+        for(auto& p : edge_paths) {
+            auto e = p.second.front();
+            if(p.second.size() == 1 && !handled[e]) {
+                long val = path_val - p.first;
+                w_map[e] += val;
+                w_map[r_map[e]] -= val;
+                handled[e] = true;
+                cout << "New handled edge " << e << " with weight: " << w_map[e] << endl;
+                finished++;
+            }
+        }
+    }
+
+//    push_relabel_max_flow(g, source, target);
+//    cycle_canceling(g)
+    successive_shortest_path_nonnegative_weights(g, source, target);
+    cout << path_val - find_flow_cost(g) << endl;
 }
 
 int main() {
